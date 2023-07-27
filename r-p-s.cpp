efd3ca3dc690a035a354b25cbe0ef6d021a9f768
Vishwaksen Reddy Dhareddy <73452224+vishwaksen-1@users.noreply.github.com>
2023-07-27 18:59:34 +0530

    Update r-p-s.cpp

--- r-p-s.cpp
+++ r-p-s.cpp
@@ -1,3 +1,20 @@
+/*This program is a Rock-Paper-Scissor game written in C++. It is a simple and fun game that can be played on the terminal. 
+The game starts with the player deciding the number of matches to play. The game follows the standard rules of Rock-Paper-Scissor: 
+rock beats scissors, scissors beat paper, and paper beats rock. The game is played between the user and the computer, who randomly 
+chooses one of the three options. Users can enter their choice by typing 1, 2, or 3 each for Rock, Paper, and Scissors. The program
+then compares the choices and displays the result. The game ends after all the rounds if the player or the computer wins. And the
+game prompts for another match when the game ends with a draw.*/
+
+/*The code is implemented in the simplest way possible, only using loops and conditionals. The code consists of a main function 
+containing the game's main logic, such as game initialization, game processing, and the game results. Each is an independent function 
+and uses multiple functions like game_func, display_move, and take_input. And the important variables storing the scores and game 
+rounds are made universal to avoid repeated copying.*/
+
+/*The code is well-structured, modular, and easy to read. It uses comments to explain the purpose and functionality of each function 
+and variable. It also uses proper indentation, spacing, and naming conventions to follow the C++ coding standards. The code 
+demonstrates good programming skills and logic.*/
+
+
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
